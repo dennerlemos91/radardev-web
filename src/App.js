8c7d6da1cb7c5169a7ff97f0082593ff02cc9440
denner <denner@stj.jus.br>
2020-01-16 18:06:10 -0300

    Criação do template inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,171 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./Global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
 
 function App() {
+  const [github_username, setGithubUsername] = useState("");
+  const [techs, setTechs] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      err => {
+        console.log(err);
+      },
+      {
+        timeout: 30000
+      }
+    );
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefaut();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input
+              name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+            ></input>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            ></input>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              ></input>
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input
+                type="number"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              ></input>
+            </div>
+          </div>
+
+          <button type="submit"> Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/46164884?s=460&v=4"
+                alt="Denner Lemos"
+              />
+              <div className="user-info">
+                <strong>Denner Lemos</strong>
+                <span>Java, Angular, Node.js, React</span>
+              </div>
+            </header>
+            <p>
+              Apaixonado por Java, JavaScript, Angular ou tudo que envolve
+              tecnologia.
+            </p>
+            <a href="https://github.com/dennerlemos91">
+              Acessar perfil no Github
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/46164884?s=460&v=4"
+                alt="Denner Lemos"
+              />
+              <div className="user-info">
+                <strong>Denner Lemos</strong>
+                <span>Java, Angular, Node.js, React</span>
+              </div>
+            </header>
+            <p>
+              Apaixonado por Java, JavaScript, Angular ou tudo que envolve
+              tecnologia.
+            </p>
+            <a href="https://github.com/dennerlemos91">
+              Acessar perfil no Github
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/46164884?s=460&v=4"
+                alt="Denner Lemos"
+              />
+              <div className="user-info">
+                <strong>Denner Lemos</strong>
+                <span>Java, Angular, Node.js, React</span>
+              </div>
+            </header>
+            <p>
+              Apaixonado por Java, JavaScript, Angular ou tudo que envolve
+              tecnologia.
+            </p>
+            <a href="https://github.com/dennerlemos91">
+              Acessar perfil no Github
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/46164884?s=460&v=4"
+                alt="Denner Lemos"
+              />
+              <div className="user-info">
+                <strong>Denner Lemos</strong>
+                <span>Java, Angular, Node.js, React</span>
+              </div>
+            </header>
+            <p>
+              Apaixonado por Java, JavaScript, Angular ou tudo que envolve
+              tecnologia.
+            </p>
+            <a href="https://github.com/dennerlemos91">
+              Acessar perfil no Github
+            </a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
